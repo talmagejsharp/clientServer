b5af72331859d00f9b16545fbd62fd885c5c036a
talmagesharp <talmagesharp321@gmail.com>
2023-11-18 18:20:44 -1000

    Initial commit

---
+++ client.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(const char *msg) // a method to print errors.
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n; // declares variables.
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) // prints errors if there aren't enough arguments.
+    {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
+    }
+
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); // creats the socket connection.
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    server = gethostbyname(argv[1]); // Gets server address.
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    bzero((char *)&serv_addr, sizeof(serv_addr)); //erases garbage values
+    serv_addr.sin_family = AF_INET;               // sets the address family to IPv4
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+    serv_addr.sin_port = htons(portno); //sets the port number
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR connecting"); //prints errors
+
+    printf("Please enter the message: ");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
+
+    n = write(sockfd, buffer, strlen(buffer)); //attempts to write the message to the socket
+    if (n < 0)
+        error("ERROR writing to socket");
+
+    bzero(buffer, 256);
+    n = read(sockfd, buffer, 255); //reads the response from the server.
+    if (n < 0)
+        error("ERROR reading from socket");
+    printf("%s\n", buffer); //prints the servers response.
+
+    close(sockfd);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void error(const char *msg) //used to print errors.
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno;
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+
+    if (argc < 2) //checks for argument length
+    {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); //creates a socket
+    if (sockfd < 0)
+        error("ERROR opening socket");
+
+    bzero((char *)&serv_addr, sizeof(serv_addr)); //trashes garbage values
+    portno = atoi(argv[1]);
+
+    serv_addr.sin_family = AF_INET;         //IPv4
+    serv_addr.sin_addr.s_addr = INADDR_ANY; //allowing any IP address to access this server
+    serv_addr.sin_port = htons(portno);     //sets port number
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) //checks for error
+        error("ERROR on binding");
+
+    listen(sockfd, 5); //listens on the port
+    clilen = sizeof(cli_addr);
+
+    while (1)
+    {
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen); //accepts 1 request from a new client
+        if (newsockfd < 0)
+            error("ERROR on accept");
+
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255); //reads in the values from the client
+        if (n < 0)
+            error("ERROR reading from socket");
+        printf("Here is the message: %s\n", buffer); //prints the values
+
+        n = write(newsockfd, "I got your message", 18); //writes to the client
+        if (n < 0)
+            error("ERROR writing to socket");
+
+        close(newsockfd); //closes the new socket
+    }
+
+    close(sockfd); //closes the port
+    return 0;
+}
