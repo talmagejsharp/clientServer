6ff39bc0b2b43892bf8709ef8307cb1db6bb409a
talmagesharp <talmagesharp321@gmail.com>
2023-11-20 13:25:26 -1000

    Add parsing for client, parses file length then reads in the file

--- client/client.c
+++ client/client.c
@@ -68,7 +68,49 @@ int main(int argc, char *argv[])
     n = read(sockfd, buffer, 255); //reads the response from the server.
     if (n < 0)
         error("ERROR reading from socket");
-    printf("%s\n", buffer); //prints the servers response.
+    char* endFlag = strstr(buffer, "<END>");
+    if (endFlag != NULL) {
+        size_t beforeEndSize = endFlag - buffer;
+        size_t afterEndSize = strlen(endFlag + strlen("<END>"));
+
+        // Allocate memory for the new strings (+1 for null terminator)
+        char* beforeEnd = (char*)malloc(beforeEndSize + 1);
+        char* afterEnd = (char*)malloc(afterEndSize + 1);
+
+        if (beforeEnd == NULL || afterEnd == NULL) {
+            // Handle memory allocation error
+        } else {
+            // Copy the parts to new strings
+            strncpy(beforeEnd, buffer, beforeEndSize);
+            beforeEnd[beforeEndSize] = '\0'; // Null-terminate the string
+
+            strcpy(afterEnd, endFlag + strlen("<END>")); // Copy the part after "<END>"
+
+            // Use 'beforeEnd' and 'afterEnd' as needed
+            // ...
+            printf("File Size is: %s\n", beforeEnd);
+            // printf("After <END>: %s\n", afterEnd);
+
+            int fileSize = atoi(beforeEnd);
+            int bytesReceived = strlen(afterEnd);
+            printf("%s", afterEnd);
+            while(bytesReceived < fileSize){
+                n = read(sockfd, buffer, 255);
+                printf("%s", buffer);
+                bytesReceived += n;
+                bzero(buffer,256);
+                // printf("Bytes Received: %d\n", bytesReceived);
+            }
+            printf("\n");
+
+            // Free the allocated memory
+            free(beforeEnd);
+            free(afterEnd);
+        }
+    }
+
+    
+    
 
     close(sockfd);
     return 0;
--- serverFiles/server.c
+++ serverFiles/server.c
@@ -5,6 +5,9 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
 
 void error(const char *msg) //used to print errors.
 {
@@ -12,15 +15,62 @@ void error(const char *msg) //used to print errors.
     exit(1);
 }
 
-void handle_request(int socket, char requestBuffer[]){
-        int n = read(socket, requestBuffer, 255); //reads in the values from the client
-        if (n < 0)
-            error("ERROR reading from socket");
-        printf("Here is the message: %s\n", requestBuffer); //prints the values
-        
-        n = write(socket, "I got your message", 18); //writes to the client
-        if (n < 0)
+void handle_request(int socket, char requestBuffer[]) {
+    int n = read(socket, requestBuffer, 255); // Reads in the values from the client
+    if (n < 0) error("ERROR reading from socket");
+
+    char* endFlag = strstr(requestBuffer, "<END>");
+    if (endFlag != NULL) {
+        *endFlag = '\0'; // Replace the start of "<END>" with a null terminator
+    }
+    printf("Here is the message: %s\n", requestBuffer); // Prints the values
+
+    // Open the requested file
+    FILE *file = fopen(requestBuffer, "rb"); // Open in binary mode
+    if (file == NULL) {
+        n = write(socket, "ERROR: File not found ", 22);
+        error("ERROR opening file");
+    }
+
+    // Get file size
+    struct stat st;
+    stat(requestBuffer, &st);
+    long fileSize = st.st_size;
+    printf("The file size is %ld bytes\n", fileSize);
+
+    // Allocate memory for file buffer
+    char *fileBuffer = (char *)malloc(fileSize);
+    if (fileBuffer == NULL) {
+        error("ERROR allocating memory");
+    }
+
+    // Read file into buffer
+    fread(fileBuffer, sizeof(char), fileSize, file);
+    if (ferror(file)) {
+        free(fileBuffer);
+        fclose(file);
+        error("ERROR reading file");
+    }
+    fclose(file);
+    char fileSizeMsg[256]; // Buffer for the file size message
+    snprintf(fileSizeMsg, sizeof(fileSizeMsg), "%ld<END>", fileSize);
+
+    n = write(socket, fileSizeMsg, strlen(fileSizeMsg));
+    if (n < 0) {
+        error("ERROR writing to socket");
+    }
+        // Send file to the client in chunks
+    long bytesSent = 0;
+    while (bytesSent < fileSize) {
+        n = write(socket, fileBuffer + bytesSent, fileSize - bytesSent);
+        if (n < 0) {
+            free(fileBuffer);
             error("ERROR writing to socket");
+        }
+        bytesSent += n;
+    }
+
+    free(fileBuffer); // Free the allocated memory
 }
 
 int main(int argc, char *argv[])
